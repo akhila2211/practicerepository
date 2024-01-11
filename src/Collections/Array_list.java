e405ab450108a7fdc866d74b946c72e276d54394
HP <HP@192.168.1.14>
2024-01-11 11:19:49 +0530

    second commit

--- src/Collections/Array_list.java
+++ src/Collections/Array_list.java
@@ -6,11 +6,11 @@ public class Array_list {
 
 	
 	 public static void main(String args[]){  
-	  ArrayList<String> list=new ArrayList<String>();
-	      list.add("A"); 
-	      list.add("K");    
-	      list.add("H");    
-	      list.add("I");    
-	      System.out.println(list);  
+	  ArrayList<String> listing=new ArrayList<String>();
+	      listing.add("A"); 
+	      listing.add("K");    
+	      listing.add("H");    
+	      listing.add("I");    
+	      System.out.println(listing);  
 	 }  
 }
